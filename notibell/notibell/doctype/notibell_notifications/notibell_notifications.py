b6afeb58cc1907504e799d230fbb9c87ee049ae4
Rajat Tripathi <rajat.tripathi@nestorbird.com>
2023-08-31 11:41:59 +0000

    Update notibell/notibell/doctype/notibell_notifications/notibell_notifications.py

--- notibell/notibell/doctype/notibell_notifications/notibell_notifications.py
+++ notibell/notibell/doctype/notibell_notifications/notibell_notifications.py
@@ -1,8 +1,45 @@
 # Copyright (c) 2023, NestorBird and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import requests
+import json
 from frappe.model.document import Document
 
 class NotiBellNotifications(Document):
 	pass
+
+@frappe.whitelist(allow_guest=True)
+def send_push_notification(device_token, title, body, key, value):
+    app_integration = frappe.get_doc("NotiBell Notifications")
+    if not app_integration.enable_app_notifications:
+        return {
+            'statusCode': 404,
+            'message': "Notification is not enable. Please enable it from App Integration"
+        }
+    if not app_integration.app_secret_key:
+        return {
+            'statusCode': 404,
+            'message': "please Insert Secret Key in App Integration"
+        }
+    url = app_integration.url
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "key={app_secret_key}".format(app_secret_key=app_integration.app_secret_key),
+    }
+    payload = {
+        "to": device_token,
+        "collapse_key": "type_a",
+        "notification": {
+            "body": "" + body,
+            "title": "" + title,
+        },
+        "data": {
+            "key": key,
+            "value": value
+        }
+    }
+    payload = json.dumps(payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
